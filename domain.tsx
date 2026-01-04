1c7fb426405a3abf43f9ec7799074d996d62fa1d
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-04 21:30:21 +0700

    Update domain config via WinForms tool

--- domain.tsx
+++ domain.tsx
@@ -1,2 +1,2 @@
-//export const domain = "https://vtus.feji.io";
-export const domain = "https://vtus.livextop.com";
+export const domain = "https://vtus.feji.io";
+//export const domain = "https://vtus.livextop.com";
