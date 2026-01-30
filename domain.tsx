f87725e296bc2bede9f46ee2cb8db2d93d3ef5cd
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-30 13:42:30 +0700

    Update domain.tsx

--- domain.tsx
+++ domain.tsx
@@ -1,3 +1,4 @@
 //export const domain = "https://vtus.cafex.biz";
 //export const domain = "https://vtus.feji.io";
-export const domain = "https://vtus.livextop.com";
+//export const domain = "https://vtus.livextop.com";
+export const domain = "https://vtus.daily24.blog";
